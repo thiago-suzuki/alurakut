768d3bcd313cc1d4f10cccee288477fdb6dd8c4e
Thiago Suzuki <thiagoikenaga@gmail.com>
2021-07-12 17:03:51 -0300

    Arrumação de Código

--- pages/index.js
+++ pages/index.js
@@ -25,41 +25,41 @@ export default function Home() {
     ]
     return ( 
       <>
-      <AlurakutMenu />
-      <MainGrid>
-        {/* <Box style="grid-area: profileArea;"> */}
-        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-          <ProfileSideBar githubUser={usuarioAleatorio}/>
-        </div>
-        
-        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
-          <Box>
-            <h1 className="title">Bem Vindo(a), {usuarioAleatorio.toUpperCase().replace('-', ' ')}</h1>
-            <OrkutNostalgicIconSet />
-          </Box>          
-        </div>
-        
-        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-          <ProfileRelationsBoxWrapper>
-            <h2 className="smallTitle">
-              Pessoas da Comunidade ({pessoasFavoritas.length})
-            </h2>
+        <AlurakutMenu />
+        <MainGrid>
+          {/* <Box style="grid-area: profileArea;"> */}
+          <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+            <ProfileSideBar githubUser={usuarioAleatorio}/>
+          </div>
+          
+          <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+            <Box>
+              <h1 className="title">Bem Vindo(a), {usuarioAleatorio.toUpperCase().replace('-', ' ')}</h1>
+              <OrkutNostalgicIconSet />
+            </Box>          
+          </div>
+          
+          <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+            <ProfileRelationsBoxWrapper>
+              <h2 className="smallTitle">
+                Pessoas da Comunidade ({pessoasFavoritas.length})
+              </h2>
 
-            <ul>
-              {pessoasFavoritas.map((itemAtual) => {
-                return (
-                  <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
-                      <img src={`https://github.com/${itemAtual}.png`} />
-                      <span>{itemAtual}</span>
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
-          </ProfileRelationsBoxWrapper>
-        </div>
-      </MainGrid>
+              <ul>
+                {pessoasFavoritas.map((itemAtual) => {
+                  return (
+                    <li>
+                      <a href={`/users/${itemAtual}`} key={itemAtual}>
+                        <img src={`https://github.com/${itemAtual}.png`} />
+                        <span>{itemAtual}</span>
+                      </a>
+                    </li>
+                  )
+                })}
+              </ul>
+            </ProfileRelationsBoxWrapper>
+          </div>
+        </MainGrid>
       </>
     )
 }
